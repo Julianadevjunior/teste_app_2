b871cd80429f43c412732c70515b6bbba45e3ddb
Julianadevjunior <jucarlos.jv@gmail.com>
2025-02-19 16:59:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ account_sid = st.secrets["TWILIO"]["account_sid"]  # Acessa a seção TWILIO
 auth_token = st.secrets["TWILIO"]["auth_token"]
 twilio_phone = st.secrets["TWILIO"]["SENHA_SECRETA"]
 
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-  from_=twilio_phone,
-  body='Mensagem enviada',
-  to='+5513996376382'
-)
+if st.button("Enviar"):
+  client = Client(account_sid, auth_token)
+  message = client.messages.create(
+    from_=twilio_phone,
+    body='Mensagem enviada',
+    to='+5513996376382'
+  )
 
 # print(os.getenv(key="account_sid"))
 # print(os.getenv(key="auth_token"))
