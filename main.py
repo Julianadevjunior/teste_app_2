9591f3c09f9ddacdbf5d22e1054389528a360363
Julianadevjunior <jucarlos.jv@gmail.com>
2025-02-23 10:34:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import streamlit as st
 account_sid = st.secrets["TWILIO"]["account_sid"]
 auth_token = st.secrets["TWILIO"]["auth_token"]
 twilio_phone = st.secrets["TWILIO"]["SENHA_SECRETA"]
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-  from_='+18777804236',
-  body='oi',
-  to='+5513996260027'
-)
-
-print(message.sid)
+st.write(account_sid, auth_token)
+# client = Client(account_sid, auth_token)
+#
+# message = client.messages.create(
+#   from_='+18777804236',
+#   body='oi',
+#   to='+5513996260027'
+# )
+#
+# print(message.sid)
