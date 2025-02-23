971bb9748cb030403a4adefb2152ee0bca119818
Julianadevjunior <jucarlos.jv@gmail.com>
2025-02-23 11:16:30 -0300

    c

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import streamlit as st
 import pandas as pd
 import os
 
-account_sid = st.secrets["TWILIO"]["account_sid"]
-auth_token = st.secrets["TWILIO"]["auth_token"]
-twilio_phone = st.secrets["TWILIO"]["SENHA_SECRETA"]
-st.write(account_sid, auth_token)
-client = Client(account_sid, auth_token)
+# account_sid = st.secrets["TWILIO"]["account_sid"]
+# auth_token = st.secrets["TWILIO"]["auth_token"]
+# twilio_phone = st.secrets["TWILIO"]["SENHA_SECRETA"]
+# st.write(account_sid, auth_token)
+# client = Client(account_sid, auth_token)
+#
+# message = client.messages.create(
+#   from_='+18575678828',
+#   body=f'{30*"oi, "}',
+#   to='+5513996260027'
+# )
 
-message = client.messages.create(
-  from_='+18575678828',
-  body=f'{30*"oi, "}',
-  to='+5513996260027'
-)
-st.write(os.listdir())
-# df = pd.read_excel("cadastros.xlsx")
-# st.dataframe(df)
+df = pd.read_excel("cadastros.xlsx")
+print(df)
+st.dataframe(df)
