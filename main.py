83e40bc7a0d4f0848e6526da647016ae1e5ba9cf
Julianadevjunior <jucarlos.jv@gmail.com>
2025-02-23 10:35:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ account_sid = st.secrets["TWILIO"]["account_sid"]
 auth_token = st.secrets["TWILIO"]["auth_token"]
 twilio_phone = st.secrets["TWILIO"]["SENHA_SECRETA"]
 st.write(account_sid, auth_token)
-# client = Client(account_sid, auth_token)
-#
-# message = client.messages.create(
-#   from_='+18777804236',
-#   body='oi',
-#   to='+5513996260027'
-# )
-#
-# print(message.sid)
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='+18777804236',
+  body='oi',
+  to='+5513996260027'
+)
+
+print(message.sid)
