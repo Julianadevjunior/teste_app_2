370fd94e2497d7682588975816d79fd4562362c0
Julianadevjunior <jucarlos.jv@gmail.com>
2025-02-23 17:51:52 -0300

    c

--- main.py
+++ main.py
@@ -4,30 +4,22 @@ import streamlit as st
 import pandas as pd
 import os
 import requests
-link_google = "https://docs.google.com/spreadsheets/d/1NMM-IHZgF3aM0kqqDcGq_iRakOt5hogzkqxSrb2zK2E/edit?usp=sharing"
-Código_de_implantação = "AKfycbxazn8Ci1UJw68ujjuXAY4PGfN-eOaTbgWWzZCwQkCogIwawe2OPWtVkRgC8KywSKo"
-url_planilha = "https://script.google.com/macros/s/AKfycbxazn8Ci1UJw68ujjuXAY4PGfN-eOaTbgWWzZCwQkCogIwawe2OPWtVkRgC8KywSKo/exec"
-# Link correto de implantação do Google Apps Script
-codigo_implantacao = "AKfycbwMnd9TxGAv0ibanXgv9fq81lNXK3v2s4DvH0RYUqSjx9jh2pCEVSuO2Sy7EDq3lmbp"
-url = f"https://script.google.com/macros/s/{codigo_implantacao}/exec"
 
-# Faz a requisição
-response = requests.get(url)
 
-# Verifica o conteúdo da resposta
-if response.status_code == 200:
-    print(response.json())
-    try:
-        data = response.json()
-        df = pd.DataFrame(data[1:], columns=data[0])  # Cabeçalho + dados
-        st.dataframe(df)
-    except requests.exceptions.JSONDecodeError:
-        st.error("Erro ao decodificar JSON. Verifique a resposta da URL.")
-else:
-    st.error(f"Erro na requisição: {response.status_code}")
+url = f"https://script.google.com/macros/s/AKfycbwMnd9TxGAv0ibanXgv9fq81lNXK3v2s4DvH0RYUqSjx9jh2pCEVSuO2Sy7EDq3lmbp/exec"
 
-# Código de implantação
-# AKfycbwMnd9TxGAv0ibanXgv9fq81lNXK3v2s4DvH0RYUqSjx9jh2pCEVSuO2Sy7EDq3lmbp
-# App da Web
-# URL
-# https://script.google.com/macros/s/AKfycbwMnd9TxGAv0ibanXgv9fq81lNXK3v2s4DvH0RYUqSjx9jh2pCEVSuO2Sy7EDq3lmbp/exec
+# # Faz a requisição
+# response = requests.get(url)
+#
+# # Verifica o conteúdo da resposta
+# if response.status_code == 200:
+#     print(response.json())
+#     try:
+#         data = response.json()
+#         df = pd.DataFrame(data[1:], columns=data[0])  # Cabeçalho + dados
+#         st.dataframe(df)
+#     except requests.exceptions.JSONDecodeError:
+#         st.error("Erro ao decodificar JSON. Verifique a resposta da URL.")
+# else:
+#     st.error(f"Erro na requisição: {response.status_code}")
+
